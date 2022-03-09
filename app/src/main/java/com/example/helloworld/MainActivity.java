520819ebd3a98d88410783f4d2dde77d5819f08e
Susan-Paudel <psusan19@tbc.edu.np>
2022-03-09 21:02:20 +0545

    homework upload

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        button=(Button) findViewById(R.id.button_zero);
     }
 
     /*
@@ -38,8 +43,17 @@ public class MainActivity extends AppCompatActivity {
      *             Since the count always appears in the TextView,
      *             the passed in view is not used.
      */
+    @SuppressLint("ResourceAsColor")
     public void countUp(View view) {
         mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+            view.setBackgroundColor(R.color.green);
+            button.setBackgroundColor(R.color.colorAccent);
+    }
+
+    public void countZero(View view) {
+        mCount=0;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
