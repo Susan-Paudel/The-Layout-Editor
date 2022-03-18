bb6b6f3d648a5c305256eeeac7d478e1a1f29c55
Susan-Paudel <psusan19@tbc.edu.np>
2022-03-18 13:08:51 +0545

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# lab work 2
+## Hello Toast and Counter(Task_3)
+
+**This is the second lab Exercise "The-Layout-Editor" and this readme file consist of the screenshot for the final app created**
+
+### Change layout to Relative Layout
+
+![linearlayout](ScreenshotAndRecord/relative.png)
+
+**the above screenshot show the relative layout of hello toast**
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.helloworld;
-
+//import required library
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,14 +7,29 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * Display two Buttons and a TextView
+ * -Clicking the Count button that increases the displayed mCount
+ * -Clicking the Toast button that show the toast message
+ */
+//MinActivity child class inherit all the property of AppCompatActivity i.e. parent
 public class MainActivity extends AppCompatActivity {
+    //private int mCount
     private int mCount = 0;
+    //TextView
     private TextView mShowCount;
 
+    /**
+     * Activities have the ability,to restore themselves to a previous state using the data stored in this bundle
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super keyword is used to refer parent class object
         super.onCreate(savedInstanceState);
+        //it set the XML file as your main layout when the app starts
         setContentView(R.layout.activity_main);
+        //taking the id from textview
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
@@ -26,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
      *             the passed in view is not used.
      */
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        //show toast
         toast.show();
     }
 
@@ -39,8 +54,11 @@ public class MainActivity extends AppCompatActivity {
      *             the passed in view is not used.
      */
     public void countUp(View view) {
+        //increase count
         mCount++;
+        //if count is not null
         if (mShowCount != null)
+            //setText to textview
             mShowCount.setText(Integer.toString(mCount));
     }
 }
