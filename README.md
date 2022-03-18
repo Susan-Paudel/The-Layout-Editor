1da3038135a13215f08bc10a7dc7faa3ab5d612f
Susan-Paudel <psusan19@tbc.edu.np>
2022-03-18 19:42:12 +0545

    file Update

---
+++ README.md
@@ -0,0 +1,67 @@
+# lab work 2
+## Hello Toast and Counter(Homework)
+
+**This is the second lab Exercise "The-Layout-Editor" and this readme file consist of the screenshot and video i.e. gif file for the final app created**
+
+### The UI
+
+![UI](ScreenshotAndRecord/homepage.png)
+
+**The screenshot above shows the final UI of the app that was created in this exercise. The UI consists of 3 buttons and a
+text view. The 'Toast' button displays a toast message and the 'Count' button increases the counter value in the text view.
+Similarly, the 'Zero' button sets the counter value to 0 when pressed.**
+
+---
+
+### Count Button Feature
+
+**This is the feature that is added to the count button where the background color of the button is set to light Green when
+the counter value is 'even' and the color is set to light blue when the counter value is 'odd'.**
+
+![Count Feature](ScreenshotAndRecord/oddnumber.png)
+
+**The background color of the count button is set to light blue when the count value is odd.**
+
+![Count Feature](ScreenshotAndRecord/even.png)
+
+**The background color of the count button is set to light green when the count value is even.**
+
+
+---
+
+### Zero Button Feature
+
+**This is the feature included with the zero button of the app. The background color of the button is set to teal when the
+counter value is greater than 1 or when it is active and when it is pressed the value is set to 0 along with the change in its background color to what it
+was before.**
+
+![Zero Button](ScreenshotAndRecord/zerobuttonclicked.png)
+
+**The background color of the zero button is set to teal referring that it is active.**
+
+![Zero Button](ScreenshotAndRecord/zerobuttonclicked.png)
+
+**The background color of the zero button is set to the original color when it is pressed. The count value is also set to 0.**
+
+---
+
+### UI in Land scape orientation
+![Land_scape](ScreenshotAndRecord/Land.png)
+
+**The above screenshot show the land scape orientation**
+
+---
+
+### UI in tablet(Large devices)
+
+![large device](ScreenshotAndRecord/large.png)
+
+**The above screenshot show the tablet view**
+
+---
+
+### Working process of App
+
+![Record](ScreenshotAndRecord/Record.gif)
+
+**The above record gif file show the working functionality of the application**
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.helloworld;
-
+//import required library
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,21 +9,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+/**
+ * Display two Buttons and a TextView
+ * -Clicking the Count button that increases the displayed mCount
+ * -Clicking the Toast button that show the toast message
+ * -clicking the count Zero button that reset the value to zero
+ */
+//MinActivity child class inherit all the property of AppCompatActivity i.e. parent
 public class MainActivity extends AppCompatActivity {
+    //assign the initial value
     private int mCount = 0;
+    //declare textview and Button
     private TextView mShowCount;
-    private Button button;
+    private Button button,count;
 
+    /**
+     * Activities have the ability,to restore themselves to a previous state using the data stored in this bundle
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super keyword is used to refer parent class object
         super.onCreate(savedInstanceState);
+        //it set the XML file as your main layout when the app starts
         setContentView(R.layout.activity_main);
+        //getting show count by id i.e.textview
         mShowCount = (TextView) findViewById(R.id.show_count);
+        //getting button by id
         button=(Button) findViewById(R.id.button_zero);
+        count=(Button)findViewById(R.id.button_count);
     }
 
-    /*
+    /**
      * Shows a Toast when the TOAST button is clicked.
      *
      * @param view The view that triggered this onClick handler.
@@ -31,12 +48,12 @@ public class MainActivity extends AppCompatActivity {
      *             the passed in view is not used.
      */
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        //show the toast message
         toast.show();
     }
 
-    /*
+    /**
      * Increments the number in the TextView when the COUNT button is clicked.
      *
      * @param view The view that triggered this onClick handler.
@@ -45,16 +62,41 @@ public class MainActivity extends AppCompatActivity {
      */
     @SuppressLint("ResourceAsColor")
     public void countUp(View view) {
+        //increase the counter value
         mCount++;
+        //if counter variable is not null
         if (mShowCount != null)
+            //it will set the value to mShowCount textview
             mShowCount.setText(Integer.toString(mCount));
-            view.setBackgroundColor(R.color.green);
-            button.setBackgroundColor(R.color.colorAccent);
+           //it will change the background color of zero button to pink
+            button.setBackgroundResource(R.color.colorAccent);
+            if(mCount % 2 == 0){
+                //even
+                view.setBackgroundResource(R.color.green);
+            }else if(mCount%2==1){
+                //it will change the background color of button of count to green
+                view.setBackgroundResource(R.color.teal_200);
+            }else{
+            //it will change the background color of button of count to green
+            view.setBackgroundResource(R.color.colorPrimary);
+        }
     }
-
+    /**
+     * reset the value of counter to zero
+     *
+     * @param view The view that triggered this onClick handler.
+     * Since the count always appears in the TextView,the passed in view is not used.
+     */
     public void countZero(View view) {
+        //store the value of mCount to zero
         mCount=0;
+        //if mShowCount is not null
         if (mShowCount != null)
+            //it will set the value to mShowCount textview
             mShowCount.setText(Integer.toString(mCount));
+           //it will change the background color of zero button to gray
+            view.setBackgroundResource(R.color.gray);
+           //it will change the background color of count button to primary color
+            count.setBackgroundResource(R.color.colorPrimary);
     }
 }
